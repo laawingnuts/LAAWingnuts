df8be4bfd10e192ddc31565147c2b7cd701982c9
laawingnuts <laawingnuts@gmail.com>
2022-12-06 11:18:55 -0800

    created practice branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_1122.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_1122.java
@@ -154,5 +154,6 @@ public class Auto_122 extends LinearOpMode {
         rightBackDrive.setPower(-1);
         leftFrontDrive.setPower(-1);
         leftBackDrive.setPower(-1);
+        //test comment
     }
 }
